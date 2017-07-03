91c3c11fc0ca415e9f711ec08db48c08d57f8fab
Stivali Serna <stivaliserna08@gmail.com>
2017-07-02 21:32:39 -0400

    Make navigation menu full responsive

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,4 +1,5 @@
 var navToggle = document.getElementsByClassName('nav-btn-toggle')[0]
+var btnRemove = document.getElementsByClassName('nav-btn-remove')[0]
 var navMenu = document.getElementsByClassName('js-site-nav')[0]
 var isMouseDown = false
 
@@ -6,6 +7,10 @@ navToggle.addEventListener('click', function () {
   this.focus()
   navMenu.classList.toggle('js-site-nav--open')
   navMenu.focus()
+})
+
+btnRemove.addEventListener('click', function () {
+  navMenu.classList.remove('js-site-nav--open')
 })
 
 navMenu.addEventListener('mousedown', function () {
